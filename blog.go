1e173b998ed34c1647dacbb547111d4195612b72
skyforce77 <usulguru@gmail.com>
2018-07-31 21:01:14 +0200

    Adding articles reading

--- blog.go
+++ blog.go
@@ -7,9 +7,24 @@ import (
 	"github.com/a8m/mark"
 	"io/ioutil"
 	"log"
+	"strings"
+)
+
+type Article struct {
+	Title    string
+	Author   string
+	Date     string
+	Content  string
+	Metadata map[string]string
+}
+
+var (
+	articles []Article
 )
 
 func main() {
+	readArticles()
+
 	core := t.Init()
 	core.SetBrand("typhoonblog")
 
@@ -17,21 +32,68 @@ func main() {
 		e.Player.SendMessage(t.ChatMessage("Welcome to my blog !"))
 	})
 
-	core.DeclareCommand(t.CommandNodeLiteral(
-		"article",
+	core.DeclareCommand(t.CommandNodeLiteral("article",
 		[]*t.CommandNode{
-			t.CommandNodeLiteral(
-				"read",
+			t.CommandNodeLiteral("read",
 				[]*t.CommandNode{
-					t.CommandNodeLiteral(
-						"all",
+					t.CommandNodeLiteral("all",
 						nil,
+						func(player *t.Player, args []string) {
+							SendArticles(player)
+						},
+					),
+					t.CommandNodeArgument("Article Id",
+						nil,
+						&t.CommandParserInteger{
+							t.OptInteger{
+								true,
+								0,
+							},
+							t.OptInteger{
+								true,
+								int32(len(articles) - 1),
+							},
+						},
 						func(player *t.Player, args []string) {
 							SendArticles(player)
 						},
 					),
 				},
 				nil,
+			),
+			t.CommandNodeLiteral("search",
+				[]*t.CommandNode{
+					t.CommandNodeArgument("sentence",
+						nil,
+						&t.CommandParserString{
+							t.CommandParserStringFormatGreedyPhrase,
+						},
+						func(player *t.Player, args []string) {
+							fmt.Println(args)
+						},
+					),
+				},
+				nil,
+			),
+			t.CommandNodeLiteral("list",
+				[]*t.CommandNode{},
+				func(player *t.Player, args []string) {
+					player.SendMessage(t.ChatMessage("Articles:"))
+
+					for i, article := range articles {
+						m := t.ChatMessage(article.Title)
+						m.SetColor(&t.ChatColorGold)
+						m.SetClickEvent(
+							t.ChatClickRunCommand(fmt.Sprintf("/article read %d", i)),
+						)
+						m.SetHoverEvent(t.ChatHoverMessage([]t.IChatComponent{
+							t.ChatMessage(article.Title),
+							t.ChatMessage(article.Author),
+							t.ChatMessage(article.Date),
+						}))
+						player.SendMessage(m)
+					}
+				},
 			),
 		},
 		nil,
@@ -40,7 +102,7 @@ func main() {
 	core.Start()
 }
 
-func SendArticles(player *t.Player) {
+func readArticles() {
 	articlesDir := "./articles"
 
 	files, err := ioutil.ReadDir(articlesDir)
@@ -53,7 +115,27 @@ func SendArticles(player *t.Player) {
 		if err != nil {
 			panic(err)
 		}
-		player.SendBukkitMessage(MinecraftRender(string(data)))
+
+		meta := generateMetadata(string(data))
+		if _, ok := meta["title"]; ok {
+			if _, ok = meta["author"]; ok {
+				if _, ok = meta["date"]; ok {
+					articles = append(articles, Article{
+						meta["title"],
+						meta["author"],
+						meta["date"],
+						string(data),
+						meta,
+					})
+				}
+			}
+		}
+	}
+}
+
+func SendArticles(player *t.Player) {
+	for _, article := range articles {
+		player.SendBukkitMessage(MinecraftRender(article.Content))
 	}
 }
 
@@ -94,4 +176,25 @@ func MinecraftRender(article string) string {
 		return ""
 	})
 	return m.Render()
+}
+
+func generateMetadata(article string) map[string]string {
+	meta := make(map[string]string)
+	m := mark.New(article, &mark.Options{
+		Smartypants: true,
+		Fractions:   true,
+	})
+	m.AddRenderFn(mark.NodeHTML, func(node mark.Node) string {
+		rnd := node.Render()
+		if strings.HasPrefix(rnd, "<!--") &&
+			strings.HasSuffix(rnd, "-->") {
+			d := strings.Split(rnd[4:len(rnd)-3], ":")
+			if len(d) >= 2 {
+				meta[strings.Trim(d[0], " ")] = strings.Trim(d[1], " ")
+			}
+		}
+		return ""
+	})
+	m.Render()
+	return meta
 }
